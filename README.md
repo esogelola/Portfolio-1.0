1054a458a1a62fbccbe18a588bf490bea8c9a113
Emmanuel Sogelola <esogelola@gmail.com>
2020-07-16 22:36:57 -0400

    ?

---
+++ README.md
@@ -0,0 +1,28 @@
+# Portfolio Website
+
+Portfolio website developed in PHP using the [CodeIgniter](https://codeigniter.com/) framework to demonstrate my skills and my projects, every project developed can be found on the website.
+
+### Check it out
+Website is live @ https://esogelola.com/
+Always maintained and updated when neccessary. If their are any issues, dont be afraid to use the contact form on the website.
+
+## Built With
+
+* [HTML]() - Standard markup language 
+* [CSS]() - Standard styling language
+* [JS](https://www.javascript.com/) - Client side scripting
+* [PHP](https://www.php.net/) - Server side scripting
+
+## Authors
+
+* **Emmanuel Sogelola** - *Initial work* - Developer
+
+
+## License
+
+This project is licensed under the MIT License - see the [license.txt](license.txt) file for details
+
+## Acknowledgments
+
+* Appreciation to the Code Igniter 3 & 4 Developers
+* jasonlong for the caynman template
---
+++ app/Views/templates/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Emmanuel Sogelola , Emmanuel, Sogelola, Olayinka, Computer Programmer"/>
+    <meta name="description" content="Emmanuel Sogelola | Currently a student at Georgebrown College studying Computer Programming."/>
+    <meta name="author" content="Emmanuel Sogelola, Ameroft, Esogelola "/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <!-- Core CSS (Bootstrap, Themify, Animate, AOS, OWL Carousel, Magnific Popup) -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" ref="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">
+    <!--Link style.css and fix.css to main page-->
+    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="static/css/fix.css">
+
+    <link rel="icon" href="favicon.ico" >
+
+    <title><?= MY_NAME ?></title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-30881260-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-30881260-2');
+    </script>
+</head>
+
+<body>
+    <!--Navigation menu-->
+    <nav class="navbar navbar-expand-lg  main-nav nav-down" id="mainNav">
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="#">E/S</a>
+
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar-main" aria-controls="navbar-main" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="ti-align-justify"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbar-main">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" data-scroll href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link" data-scroll href="#projects">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" target="_blank" data-scroll href="static/Emmanuel-Sogelola-Resume.pdf">Resume</a></li>
+                    <li class="nav-item"><a class="nav-link" data-scroll href="#Contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
